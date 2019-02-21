0754088c61090a6ba2ff8acbc45d4680192ea392
Matija Kejzar <matija.kejzar@marand.si>
2019-02-22 00:36:39 +0100

    Added re-exports.

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,21 @@ extern crate pest;
 #[macro_use]
 extern crate pest_derive;
 
+pub use pest::error::Error;
+
+pub use self::parser::parse;
+pub use self::parser::Rule;
+pub use self::uci::CommunicationDirection;
+pub use self::uci::MessageList;
+pub use self::uci::UciFen;
+pub use self::uci::UciMessage;
+pub use self::uci::UciMove;
+pub use self::uci::UciPiece;
+pub use self::uci::UciSearchControl;
+pub use self::uci::UciSquare;
+// Re–exports
+pub use self::uci::UciTimeControl;
+
 pub mod uci;
 pub mod parser;
 
